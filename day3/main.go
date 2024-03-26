1710a80bd2d08df45e6c53a3329df23867639a21
= <=>
2024-03-26 07:11:46 +0200

    Trying to figure out how resolve the day 3 challange.

--- day3/main.go
+++ day3/main.go
@@ -14,26 +14,18 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Println(puzzle1(strings.Split(string(file), "\r\n")))
+	fmt.Println(puzzle2(strings.Split(string(file), "\r\n")))
 }
 
 func puzzle1(data []string) int {
-	bytes := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
-	for _, v := range data {
-		for i := 0; i < len(v); i++ {
-			// number, err := strconv.Atoi(string(v[i]))
-			number, err := strconv.ParseInt(string(v[i]), 2, 32)
-			if err != nil {
-				log.Fatal(err)
-			}
-			if number == 1 {
-				bytes[i]++
-			}
-		}
+	bytes := []string{"0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0"}
+	for i := 0; i < len(bytes); i++ {
+		bytes[i] = CommonByte(data, i, true)
 	}
 	var gamma int32
 	var epsilon int32
 	for i := 0; i < len(bytes); i++ {
-		if bytes[i] > len(data)/2 {
+		if bytes[i] == "1" {
 			gamma = gamma | (1 << (len(bytes) - 1 - i))
 		} else {
 			epsilon = epsilon | (1 << (len(bytes) - 1 - i))
@@ -42,7 +34,63 @@ func puzzle1(data []string) int {
 	return int(gamma) * int(epsilon)
 }
 func puzzle2(data []string) int {
-	selectedBytes := []string{}
+	selectedBytes := data
+	for i := 0; len(selectedBytes) > 1; i++ {
+		commonByte := CommonByte(selectedBytes, i, true)
+		selectedBytes = getBytes(selectedBytes, i, commonByte)
+	}
+	tmp := selectedBytes[0]
+	var oxygen int64
+	for i := 0; i < 12; i++ {
+		if string(tmp[i]) == "1" {
+			oxygen = oxygen | (1 << (len(tmp) - i))
+		}
+	}
+	oxygen, _ = strconv.ParseInt(tmp, 2, 64)
+	fmt.Println(tmp)
+	selectedBytes = data
+	for i := 0; len(selectedBytes) > 1; i++ {
+		commonByte := CommonByte(selectedBytes, i, false)
+		selectedBytes = getBytes(selectedBytes, i, commonByte)
+	}
+	tmp = selectedBytes[0]
+	var co2 int64 = 0
+	for i := 0; i < 12; i++ {
+		if string(tmp[i]) == "1" {
+			co2 = co2 | (1 << (len(tmp) - i))
+		}
+	}
+	fmt.Println()
+	co2, _ = strconv.ParseInt(tmp, 2, 64)
+	fmt.Println(tmp)
+	fmt.Println(oxygen, co2)
+	return int(oxygen) * int(co2)
+}
 
-	return 0
+func CommonByte(data []string, position int, mostCommon bool) string {
+	count := 0
+	for _, v := range data {
+		if string(v[position]) == "1" {
+			count++
+		}
+	}
+	if count >= len(data)/2 && mostCommon {
+		return "1"
+	} else if count <= len(data)/2 && !mostCommon {
+		return "0"
+	} else if count < len(data)/2 && mostCommon {
+		return "0"
+	} else if count > len(data)/2 && !mostCommon {
+		return "1"
+	}
+	return "0"
+}
+func getBytes(data []string, position int, bite string) []string {
+	bytes := []string{}
+	for _, v := range data {
+		if string(v[position]) == bite {
+			bytes = append(bytes, string(v))
+		}
+	}
+	return bytes
 }
